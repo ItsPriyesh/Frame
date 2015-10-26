9842cfaf2a38ee0f9c8ad9cb5041d8590e6a4753
ItsPriyesh <priyesh.96@hotmail.com>
2015-10-25 21:47:00 -0700

    Start on file uploading

--- src/main/scala/me/priyesh/frame/FrameServlet.scala
+++ src/main/scala/me/priyesh/frame/FrameServlet.scala
@@ -1,17 +1,22 @@
 package me.priyesh.frame
 
-import org.scalatra._
-import scalate.ScalateSupport
+import org.scalatra.servlet.{MultipartConfig, FileUploadSupport}
 
-class FrameServlet extends FrameStack {
+class FrameServlet extends FrameStack with FileUploadSupport {
+
+  configureMultipartHandling(MultipartConfig(maxFileSize = Some(3 * 1024 * 1024)))
+
+  private val UploadedFileKey: String = 0xf00d toString
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    <form method="post" action="/upload" enctype="multipart/form-data">
+      <input type="file" name={UploadedFileKey}/>
+      <input type="submit"/>
+    </form>
+  }
+
+  post("/upload") {
+    print(fileParams(UploadedFileKey).getSize)
   }
 
 }
