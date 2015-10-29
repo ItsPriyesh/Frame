331b321d09775c869772795c5bc06e9874ae0376
ItsPriyesh <priyesh.96@hotmail.com>
2015-10-28 23:29:06 -0700

    Style dropzone

--- src/main/scala/me/priyesh/frame/FrameServlet.scala
+++ src/main/scala/me/priyesh/frame/FrameServlet.scala
@@ -15,15 +15,14 @@ class FrameServlet extends FrameStack with FileUploadSupport {
         <meta charset="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title>Bootstrap 101 Template</title>
+        <title>Frame</title>
         <link href="css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/styles.css" rel="stylesheet"/>
       </head>
       <body>
-        <h1>Hello, world!</h1>
 
-        <form action="/file-upload"
-              class="dropzone"
-              id="my-awesome-dropzone"></form>
+        <form action="upload" class="dropzone vertical-center dropzone-background" method="post" enctype="multipart/form-data">
+        </form>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
